4265309e6bc51078b16532d3e49bd9bef0117608
mehrdadzeinali <mehrdad.zeinali@epitech.eu>
2026-03-25 15:26:54 +0100

    Add Login and guards with JWT token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './auth.strategy';
+import { JwtGuard } from './auth.guard';
+import { UsersRepository } from 'src/users/users.repository';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '7d' },
+    }),
+],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, JwtGuard, UsersRepository, PrismaService],
+  exports: [JwtGuard]
 })
 export class AuthModule {}
