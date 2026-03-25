872a869b314ed259a59a1cc7e4d609e3e216ff4e
Anthony <anthony.guerrand92@gmail.com>
2026-03-25 15:52:51 +0100

    addOrder dynamic userId

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,21 +1,22 @@
-import { Controller, HttpCode, HttpStatus, Post, Get, Body, Param } from "@nestjs/common";
+import { Controller, HttpCode, HttpStatus, Post, Get, Body, Param, UseGuards, Request } from "@nestjs/common";
 import { OrderDto } from "./orders.dto";
 import { OrdersService } from "./orders.service";
+import { JwtGuard } from "src/auth/auth.guard";
 
+@UseGuards(JwtGuard)
 @Controller('orders')
 export class OrdersController {
-    constructor(private readonly ordersService: OrdersService) {}
+  constructor(private readonly ordersService: OrdersService) {}
 
-    @Post()
-    @HttpCode(HttpStatus.NO_CONTENT)
-    async addOrder(@Body() input: OrderDto) {
-        // TODO récupérer userId dynamiquement
-        const userId = 1; 
-        return await this.ordersService.create(userId, input);
-    }
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async addOrder(@Body() input: OrderDto, @Request() req) {
+    const userId = req.user.id;
+    return await this.ordersService.create(userId, input);
+  }
 
-    @Get(':orderNumber')
-    async getOrderByOrderNumber(@Param('orderNumber') orderNumber: string) {
-        return await this.ordersService.getOrderbyOrderNumber(orderNumber);
-    }
+  @Get(':orderNumber')
+  async getOrderByOrderNumber(@Param('orderNumber') orderNumber: string) {
+    return await this.ordersService.getOrderbyOrderNumber(orderNumber);
+  }
 }
